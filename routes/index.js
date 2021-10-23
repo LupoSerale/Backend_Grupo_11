d083bec0c60e8ef59be2df11e386139346200e42
Mileidis <milem19@hotmail.com>
2021-10-23 18:27:29 -0500

    fusion index file

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-    productoRoutes: require('./producto.routes'),
-
     ventaRoutes: require('./venta.routes'),
-
-    usersRouter: require('./users.router')
-
-=======
     productsRouter: require('./products.router'),
     usersRouter: require('./users.router'),
     authRouter: require('./auth.router')
->>>>>>> feature/auth_jwt
 }
