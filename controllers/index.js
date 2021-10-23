d3c299b76d6c95a8a269b9627c76dec214cd9474
Mileidis <milem19@hotmail.com>
2021-10-23 18:30:47 -0500

    fusion index controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,6 @@
 module.exports = {
-<<<<<<< HEAD
-    productoController: require('./producto.controller'),
-
     ventaController: require('./venta.controller'),
-
-    usersController: require('./users.controller')
-
-
-=======
     productsController: require('./products.controller'),
     usersController: require('./users.controller'),
     authController: require('./auth.controller')
->>>>>>> feature/auth_jwt
 }
