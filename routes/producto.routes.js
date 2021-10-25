323846e4736f6812242f522aad20911acf8c5fce
LupoSerale <jwcalderon2@misena.edu.co>
2021-10-25 07:51:51 -0500

    Se retira verificacion Token

--- routes/producto.routes.js
+++ routes/producto.routes.js
@@ -2,26 +2,24 @@ const { Router } = require('express');
 const router = Router();
 const { body } = require('express-validator');
 const { productoController } = require('../controllers');
-const verificarToken = require('../middlewares/verificarToken');
+//const verificarToken = require('../middlewares/verificarToken');
 
-router.get('/:id', verificarToken, productoController.obtenerProducto);
+router.get('/:id', productoController.obtenerProducto);
 
 router.get('/', productoController.obtenerProductos);
 
 router.post('/',
-    verificarToken,
     body('valor', 'El valor del producto es requerido y debe ser numerico').exists().isNumeric(),
     body('descripcion', 'La descripción del producto es requerida').exists(),
     body('estado', 'El estado del producto es requerido(true/false)').isBoolean().exists(),
     productoController.crearProducto);
 
 router.put('/:id',
-    verificarToken,
     body('valor', 'El valor del producto es requerido y debe ser numerico').exists().isNumeric(),
     body('descripcion', 'La descripción del producto es requerida').exists(),
     body('estado', 'El estado del producto es requerido(true/false)').isBoolean().exists(),
     productoController.actualizarProducto);
 
-router.delete('/:id', verificarToken, productoController.eliminarProducto);
+router.delete('/:id', productoController.eliminarProducto);
 
 module.exports = router;
